4cf93000317741cbbf7903ed3cd98cf8035fc33c
shawn yu <shawn.yu5@hotmail.com>
2023-06-10 17:17:15 -0400

    fix: only put executables in dist/

--- script/build.sh
+++ script/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 tag_name=$1
+GOOS_GOARCH='linux-amd64'
 
 mkdir dist
 cargo build --release --target x86_64-unknown-linux-gnu
-cp target/x86_64-unknown-linux-gnu/release/gh-ac gh-ac
-# target/x86_64-unknown-linux-gnu/release/gh-ac
-cp target/x86_64-unknown-linux-gnu/release/gh-ac dist/gh-ac_${tag_name}_x86_64-unknown-linux-gnu
+# cp target/x86_64-unknown-linux-gnu/release/gh-ac gh-ac
+
+cp target/x86_64-unknown-linux-gnu/release/gh-ac dist/gh-ac_${tag_name}_$GOOS_GOARCH
 # echo "TODO implement this script."
 # echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
 # exit 1
