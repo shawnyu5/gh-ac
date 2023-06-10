e4d83fb0978c3143a18821d046ba2e8206f0a67f
shawn yu <shawn.yu5@hotmail.com>
2023-06-10 16:53:42 -0400

    ci: add workflow precompile step

--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cargo --version
-cargo build --release
+tag_name=$1
+cargo build --release --target x86_64-unknown-linux-gnu
 cp target/release/gh-ac gh-ac
-cp target/release/gh-ac dist/gh-ac
+cp target/release/gh-ac dist/gh-ac_${tag_name}_x86_64-unknown-linux-gnu
 # echo "TODO implement this script."
 # echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
 # exit 1
