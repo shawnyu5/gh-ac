69b5ca08be5768046b3793ca3ab30fe33f97f85d
shawn yu <shawn.yu5@hotmail.com>
2023-06-10 17:06:23 -0400

    fix(build script): executable path

--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 tag_name=$1
 cargo build --release --target x86_64-unknown-linux-gnu
-cp target/release/gh-ac gh-ac
-cp target/release/gh-ac dist/gh-ac_${tag_name}_x86_64-unknown-linux-gnu
+cp target/release/x86_64-unknown-linux-gnu/gh-ac gh-ac
+cp target/x86_64-unknown-linux-gnu/release/gh-ac dist/gh-ac_${tag_name}_x86_64-unknown-linux-gnu
 # echo "TODO implement this script."
 # echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
 # exit 1
