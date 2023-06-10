c63a6786f466635a49e5e4d2e97cb2149d88ddea
shawn yu <shawn.yu5@hotmail.com>
2023-06-10 17:10:40 -0400

    fix(build script): create dist dir first

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 tag_name=$1
+
+mkdir dist
 cargo build --release --target x86_64-unknown-linux-gnu
-cp target/release/x86_64-unknown-linux-gnu/gh-ac gh-ac
+cp target/x86_64-unknown-linux-gnu/release/gh-ac gh-ac
+# target/x86_64-unknown-linux-gnu/release/gh-ac
 cp target/x86_64-unknown-linux-gnu/release/gh-ac dist/gh-ac_${tag_name}_x86_64-unknown-linux-gnu
 # echo "TODO implement this script."
 # echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
