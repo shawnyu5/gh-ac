18983b46b204c0cb46a57105536db5ae0a53ed9f
shawn yu <shawn.yu5@hotmail.com>
2023-06-29 20:44:08 -0400

    feat:init

--- src/git.rs
+++ src/git.rs
@@ -2,54 +2,71 @@ use anyhow::Result;
 use log::debug;
 use std::process::Command;
 
-/// git push
-pub fn push<'a>(force: bool) -> Result<()> {
-    let args = {
-        if force {
-            vec!["push", "--force"]
-        } else {
-            vec!["push"]
-        }
-    };
-    debug!("git push args: {}", args.join(" "));
-    let output = Command::new("git").args(args).output()?;
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    let stderr = String::from_utf8_lossy(&output.stderr);
-    // debug!("git push stdout: {}", &stdout);
-    // debug!("git push stderr: {}", &stderr);
-    println!("{}", stdout);
-    println!("{}", stderr);
-    return Ok(());
+pub struct Git {
+    quiet: bool,
 }
 
-/// check if there are any staged files
-///
-/// return true if there are staged files. False other wise
-pub fn check_staged_files() -> bool {
-    let output = Command::new("git")
-        .arg("diff")
-        .arg("--staged")
-        .arg("--name-only")
-        .output()
-        .unwrap();
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    println!("{}", stdout);
-    if stdout == "" {
-        return false;
-    } else {
-        return true;
+impl Git {
+    /// construct a new Git instance
+    ///
+    /// * `quiet`: if true, then git commands will be run with the `-q` flag
+    pub fn new(quiet: bool) -> Git {
+        Git { quiet }
     }
-}
+    fn construct_command(&self, args: &Vec<&str>) -> Command {
+        if self.quiet {
+            let mut cmd = Command::new("git");
+            cmd.args(args).arg("-q");
+            return cmd;
+        } else {
+            let mut cmd = Command::new("git");
+            cmd.args(args);
+            return cmd;
+        }
+    }
+    /// git push
+    pub fn push<'a>(&self, force: bool) -> Result<()> {
+        let args = {
+            if force {
+                vec!["push", "--force"]
+            } else {
+                vec!["push"]
+            }
+        };
+        debug!("git push args: {}", args.join(" "));
+        let output = self.construct_command(&args).output()?;
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        println!("{}", stdout);
+        println!("{}", stderr);
+        return Ok(());
+    }
 
-/// `git commit --amend --no-edit`
-pub fn commit_amend_no_edit() -> Result<()> {
-    //'git commit --amend --no-edit && git push --force'
+    /// check if there are any staged files
+    ///
+    /// return true if there are staged files. False other wise
+    pub fn check_staged_files(&self) -> bool {
+        let output = self
+            .construct_command(&vec!["diff", "--staged", "--name-only"])
+            .output()
+            .unwrap();
 
-    let args = vec!["commit", "--amend", "--no-edit"];
-    // let output = Command::new("git").arg("push").arg("--force").output()?;
-    let output = Command::new("git").args(args).output()?;
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    println!("{}", stdout);
-    return Ok(());
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        println!("{}", stdout);
+        if stdout == "" {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /// `git commit --amend --no-edit`
+    pub fn commit_amend_no_edit(&self) -> Result<()> {
+        let output = self
+            .construct_command(&vec!["commit", "--amend", "--no-edit"])
+            .output()?;
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        println!("{}", stdout);
+        return Ok(());
+    }
 }
