a347ca04c05e5eb628afc0103ce67ce3375e77e2
shawn yu <shawn.yu5@hotmail.com>
2023-07-26 23:48:36 -0400

    fix(ci): commit changes after compiling binary
    So cargo.lock version is also updated

--- script/build.sh
+++ script/build.sh
@@ -6,7 +6,6 @@ rm -f gh-ac
 mkdir dist
 # cargo build --release --target x86_64-unknown-linux-gnu
 RUSTFLAGS="-C target-feature=+crt-static" cargo build --target x86_64-unknown-linux-gnu --release
-# cp target/x86_64-unknown-linux-gnu/release/gh-ac gh-ac
 
 cp target/x86_64-unknown-linux-gnu/release/gh-ac dist/gh-ac_${tag_name}_$GOOS_GOARCH
 cp target/x86_64-unknown-linux-gnu/release/gh-ac .
