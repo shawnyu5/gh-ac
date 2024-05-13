e3012ef85e09d920bf0e0ab0e453565951eb206f
Shawn Yu <shawn.yu@fundSERV.com>
2024-05-13 15:02:24 -0400

    chore: remove print statements

--- golang/cmd/root.go
+++ golang/cmd/root.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"github.com/charmbracelet/log"
-	"github.com/shawnyu5/gh-ac/git"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -19,10 +18,8 @@ var RootCmd = &cobra.Command{
 	Use:   "ac",
 	Short: "Fire off Github action workflow runs, and open it in the browser",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		fmt.Println(git.CurrentBranchName())
 		if flags.debug {
 			log.SetLevel(log.DebugLevel)
-
 		}
 	},
 }
